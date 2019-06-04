25dc01c651e68895a6fd82869762ddcadf7791f7
Mem-Dixy <spammeifyoucan@me.com>
2019-06-04 10:28:38 +0000

    Update organization.
    So much for 'final'. Maybe this way is better.
    
    
    Former-commit-id: d74f8a87315f3b3506c97d6e4882567f944bc38e

---
+++ canvas/font/prepare.js
@@ -0,0 +1,15 @@
+/*
+    JSLint edition 2019-01-31
+*/
+/*jslint
+    browser:true
+*/
+/*global
+    character, character_code, character_name
+*/
+/*property
+    code, name
+*/
+var font = {};
+/* prepare your font here */
+font.rune = {}
