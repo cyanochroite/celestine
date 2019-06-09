5d4d9d96c1f7c1e4f46fb967dd6ab3d01b1094a5
Mem-Dixy <spammeifyoucan@me.com>
2019-06-09 02:24:03 +0000

    Removed JSDoc validation.
    I hate comment validation and it was depricated anyways.
    
    
    Former-commit-id: 661ae0526539353941d24f402fc1218c911a3f7f

--- canvas/event/index.js
+++
@@ -1,9 +0,0 @@
-const happy = function happy () {
-
-    "use strict";
-
-    return "stuff";
-
-};
-
-happy();
---
+++ canvas/event/listener.js
@@ -0,0 +1,76 @@
+/**
+ * @class
+ */
+class listener {
+
+
+    /**
+     */
+    constructor () {
+
+        this.type = "";
+        this.listener = null;
+        this.useCapture = false;
+
+    }
+
+
+    /**
+     * @param {string} type
+     * @returns {void}
+     */
+    dippers (type) {
+
+        this.type = type;
+
+    }
+
+
+    /**
+     * @returns {void}
+     */
+    useBubble () {
+
+        this.useCapture = false;
+
+    }
+
+
+    /**
+     * @returns {void}
+     */
+    FuseCapture () {
+
+        this.useCapture = true;
+
+    }
+
+
+    /**
+     * @param {(argument: Event) => void} method
+     */
+    useEvent (method) {
+
+
+        /**
+         * @param {Event} event
+         */
+        this.listener = (event) => method(event);
+
+    }
+
+    /**
+     * @param {(argument: EventTarget) => void} method
+     */
+    useTarget (method) {
+
+
+        /**
+         * @param {Event} event
+         */
+        this.listener = (event) => method(event.currentTarget);
+
+    }
+
+
+}
