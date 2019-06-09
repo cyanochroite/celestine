a48df46ae9674e0072b0f379d9b61f85efddda44
Mem-Dixy <spammeifyoucan@me.com>
2019-06-09 03:13:04 +0000

    ESLint config update.
    Removed all deprecated variables and sorted the list.
    
    
    Former-commit-id: 4e36d3aafdc11eec68d40b90696374c16defed50

--- canvas/event/prepare.js
+++ canvas/event/prepare.js
@@ -1,29 +1,37 @@
-function event__add (selected, event_listener) {
-
-    "use strict";
-
-    let {listener} = event_listener,
-     {type} = event_listener,
-     {use_capture} = event_listener;
-
-    selected.forEach((element) => {
-        element.addEventListener(type, listener, use_capture);
-    });
-
-}
-
 /**
- * @param {Element[]} selected
- * @param {(element: Element) => void} method
- * @returns {void}
+ * @param {{ forEach: (arg0: (element: any) => void) => void; }} selected
+ * @param {any} eventListener
  */
-function event__run (selected, method) {
+const eventAdd = function eventAdd (selected, eventListener) {
 
-    selected.forEach((element) => {
-        method(element);
-    });
+        "use strict";
 
-}
+        const {listener} = eventListener,
+            {type} = eventListener,
+            {useCapture} = eventListener;
+
+        selected.forEach((element) => {
+
+            element.addEventListener(type, listener, useCapture);
+
+        });
+
+        },
+
+    /**
+     * @param {Element[]} selected
+     * @param {(element: Element) => void} method
+     * @returns {void}
+     */
+    eventRun = function eventRun (selected, method) {
+
+        selected.forEach((element) => {
+
+            method(element);
+
+        });
+
+    };
 
 /**
  * @param {string} selector
@@ -31,10 +39,10 @@ function event__run (selected, method) {
  */
 function event__select_all (selector) {
 
-    let array = [];
+    const array = [];
 
-    
-return array.slice.call(document.querySelectorAll(selector));
+
+    return array.slice.call(document.querySelectorAll(selector));
 
 }
 
@@ -45,144 +53,5 @@ return array.slice.call(document.querySelectorAll(selector));
 function event__select_one (selector) {
 
     return [document.querySelector(selector)];
-
-}
-
-
-function event__listener_make () {
-
-    let event_listener = {};
-
-    event_listener.type = "";
-
-    /**
-     * @param {Event} event
-     */
-    event_listener.listener = (event) => event;
-    event_listener.use_capture = false;
-    
-return event_listener;
-
-}
-
-/**
- * @param {string} type
- * @param {{ type: string; }} event_listener
- */
-function event__listener_type (event_listener, type) {
-
-    event_listener.type = type;
-
-}
-
-/**
- * @param {{ use_capture: boolean; }} event_listener
- */
-function event__listener_use_bubble (event_listener) {
-
-    event_listener.use_capture = false;
-
-}
-
-/**
- * @param {{ use_capture: boolean; }} event_listener
- */
-function event__listener_use_capture (event_listener) {
-
-    event_listener.use_capture = false;
-
-}
-
-/**
- * @param {{ listener: (event: Event) => void; }} event_listener
- * @param {(argument: Event) => void} method
- */
-function event__listener_use_event (event_listener, method) {
-
-
-    /**
-     * @param {Event} event
-     */
-    event_listener.listener = (event) => method(event);
-
-}
-
-/**
- * @param {{ listener: (event: Event) => void; }} event_listener
- * @param {(argument: EventTarget) => void} method
- */
-function event__listener_use_target (event_listener, method) {
-
-
-    /**
-     * @param {Event} event
-     */
-    event_listener.listener = (event) => method(event.currentTarget);
-
-}
-
-
-class listener {
-
-    constructor () {
-
-        this._type = "";
-
-        /**
-         * @param {Event} event
-         */
-        // This._listener = (event) => (event);
-        this._use_capture = false;
-    
-}
-
-
-    /**
-     * @param {string} type
-     */
-    type (type) {
-
-        this._type = type;
-    
-}
-
-    use_bubble () {
-
-        this._use_capture = false;
-    
-}
-
-    use_capture () {
-
-        this._use_capture = true;
-    
-}
-
-
-    /**
-     * @param {(argument: Event) => void} method
-     */
-    use_event (method) {
-
-
-        /**
-         * @param {Event} event
-         */
-        this._listener = (event) => method(event);
-    
-}
-
-    /**
-     * @param {(argument: EventTarget) => void} method
-     */
-    use_target (method) {
-
-
-        /**
-         * @param {Event} event
-         */
-        this._listener = (event) => method(event.currentTarget);
-    
-}
 
 }
