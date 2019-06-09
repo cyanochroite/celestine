4e36d3aafdc11eec68d40b90696374c16defed50
Mem-Dixy <spammeifyoucan@me.com>
2019-06-09 03:13:04 +0000

    ESLint config update.
    Removed all deprecated variables and sorted the list.

---
+++ canvas/event/event.js
@@ -0,0 +1,75 @@
+/**
+ * @class
+ */
+class event {
+
+
+    /**
+     */
+    constructor () {
+
+        this.type = "";
+        this.listener = null;
+        this.useCapture = false;
+
+    }
+
+    /**
+     * @param {Element[]} selected
+     * @param {listener} eventListener
+     */
+    static add (selected, eventListener) {
+
+        selected.forEach((element) => {
+
+            element.addEventListener(
+                eventListener.type,
+                eventListener.listener,
+                eventListener.useCapture
+            );
+
+        });
+
+    }
+
+
+    /**
+     * @param {Element[]} selected
+     * @param {(element: Element) => void} method
+     * @returns {void}
+     */
+    static run (selected, method) {
+
+        selected.forEach((element) => {
+
+            method(element);
+
+        });
+
+    }
+
+
+    /**
+     * @param {string} selector
+     * @returns {Element[]} selected
+     */
+    static selectAll (selector) {
+
+        const array = [];
+
+
+        return array.slice.call(document.querySelectorAll(selector));
+
+    }
+
+    /**
+     * @param {string} selector
+     * @returns {Element[]} selected
+     */
+    static selectOne (selector) {
+
+        return [document.querySelector(selector)];
+
+    }
+
+}
