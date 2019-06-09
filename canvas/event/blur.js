4f0b2820b50e12bba5e4fd1a9bc6e107a71e57b0
Mem-Dixy <spammeifyoucan@me.com>
2019-06-09 22:19:21 +0000

    Massive error influx.
    Parser failed to find errors in previous files. Fixing the index page for starters.

--- canvas/event/blur.js
+++ canvas/event/blur.js
@@ -1,59 +1,68 @@
-/*
-    JSLint edition 2019-01-31
-*/
-/*jslint
-    browser:true
-*/
-/*global
-    element_select_all, element_select_one, event__add, event__blur,
-    event__blur_all_now, event__blur_all_run, event__blur_one_now,
-    event__blur_one_run, event__run, make_event__listener
-*/
-/*property
-*/
+/**
+ * @class
+ */
+// eslint-disable-next-line no-unused-vars
+class eventBlur {
+
+}
+
 /**
  * @param {Element[]} selected
  * @param {(event: Event | EventTarget) => void} method
  */
-function event__blur(selected, method) {
+function event__blur (selected, method) {
+
     "use strict";
-    var event_listener = event__listener_make();
+    let event_listener = event__listener_make();
     event__listener_type(event_listener, "blur");
     event__listener_use_bubble(event_listener);
     event__listener_use_target(event_listener, method);
     event__add(selected, event_listener);
+
 }
+
 /**
  * @param {string} selector
  * @param {(event: Event | EventTarget) => void} method
  */
-function event__blur_all_now(selector, method) {
-    var selected = event__select_all(selector);
+function event__blur_all_now (selector, method) {
+
+    let selected = event__select_all(selector);
     event__blur(selected, method);
     event__run(selected, method);
+
 }
+
 /**
  * @param {string} selector
  * @param {(event: Event | EventTarget) => void} method
  */
-function event__blur_all_run(selector, method) {
-    var selected = event__select_all(selector);
+function event__blur_all_run (selector, method) {
+
+    let selected = event__select_all(selector);
     event__blur(selected, method);
+
 }
+
 /**
  * @param {string} selector
  * @param {(event: Event | EventTarget) => void} method
  */
-function event__blur_one_now(selector, method) {
-    var selected = event__select_one(selector);
+function event__blur_one_now (selector, method) {
+
+    let selected = event__select_one(selector);
     event__blur(selected, method);
     event__run(selected, method);
+
 }
+
 /**
  * @param {string} selector
  * @param {(event: Event | EventTarget) => void} method
  */
-function event__blur_one_run(selector, method) {
-    var selected = event__select_one(selector);
+function event__blur_one_run (selector, method) {
+
+    let selected = event__select_one(selector);
     event__blur(selected, method);
+
 }
