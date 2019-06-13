f6cb0ff34daafdf04c101d714635951529685d68
Mem-Dixy <spammeifyoucan@me.com>
2019-06-13 22:11:31 +0000

    Event redo mess.
    Trying new idea. Saving so I don't loose stuff.
    
    
    Former-commit-id: eb211cbb8ddeee74936b7a971c04f95c786a099b

---
+++ canvas/event/EventListenerBuilder.js
@@ -0,0 +1,125 @@
+import {EventListener} from "./EventListener";
+
+/**
+ * @class
+ */
+export class EventListenerBuilder {
+
+
+    /**
+     * @param {(argument: Event | EventTarget) => void} method
+     * @returns {(event: Event) => void}
+     */
+    static listenerUseEvent (method) {
+
+
+        return (event) => method(event);
+
+    }
+
+
+    /**
+     * @param {(argument: Event | EventTarget) => void} method
+     * @returns {(event: Event) => void}
+     */
+    static listenerUseTarget (method) {
+
+        return (event) => method(event.currentTarget);
+
+    }
+
+
+    /**
+     * @param {(argument: Event | EventTarget) => void} method
+     * @param {string} [type]
+     * @returns {EventListener}
+     */
+    static makeUsingEventAndBubble (method, type) {
+
+        const
+            listener = EventListenerBuilder.listenerUseEvent(method),
+            useCapture = EventListenerBuilder.propagationUseBubble();
+        return new EventListener(type, listener, useCapture);
+
+    }
+
+
+    /**
+     * @param {string} type
+     * @param {(argument: Event | EventTarget) => void} method
+     * @returns {EventListener}
+     */
+    static makeUsingEventAndCapture (type, method) {
+
+        const
+            listener = EventListenerBuilder.listenerUseEvent(method),
+            useCapture = EventListenerBuilder.propagationUseCapture();
+        return new EventListener(type, listener, useCapture);
+
+    }
+
+
+    /**
+     * @param {string} type
+     * @param {(argument: Event | EventTarget) => void} method
+     * @returns {EventListener}
+     */
+    static makeUsingTargetAndBubble (type, method) {
+
+        const
+            listener = EventListenerBuilder.listenerUseTarget(method),
+            useCapture = EventListenerBuilder.propagationUseBubble();
+        return new EventListener(type, listener, useCapture);
+
+    }
+
+
+    /**
+     * @param {string} type
+     * @param {(argument: Event | EventTarget) => void} method
+     * @returns {EventListener}
+     */
+    static makeUsingTargetAndCapture (type, method) {
+
+        const
+            listener = EventListenerBuilder.listenerUseTarget(method),
+            useCapture = EventListenerBuilder.propagationUseCapture();
+        return new EventListener(type, listener, useCapture);
+
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    static propagationUseBubble () {
+
+        return false;
+
+    }
+
+
+    /**
+     * @returns {boolean}
+     */
+    static propagationUseCapture () {
+
+        return true;
+
+    }
+
+
+    static makeUsingEventAndBubble (method, type) {
+    }
+
+    static makeUsingEventAndCapture (type, method) {
+    }
+
+
+    static makeUsingTargetAndBubble (type, method) {
+    }
+
+    static makeUsingTargetAndCapture (type, method) {
+    }
+
+
+}
