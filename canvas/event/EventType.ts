48b0778ce856c621e39e6e5e95c445f22eab27c2
Mem-Dixy <spammeifyoucan@me.com>
2019-07-05 19:41:20 +0000

    Made EventType an enum.
    Former-commit-id: 3576c7c265f972412e6665e3289fe75241b86d08

--- canvas/event/EventType.ts
+++ canvas/event/EventType.ts
@@ -1,255 +1,240 @@
-class EventType {
-    AnimationEvent: {
-        animationcancel: 0x00,
-        animationend: 0x01,
-        animationiteration: 0x02,
-        animationstart: 0x03
-    };
-    AudioProcessingEvent: {
-        audioprocess: 0x00
-    };
-    BeforeUnloadEvent: {
-        beforeunload: 0x00
-    };
-    ClipboardEvent: {
-        copy: 0x00,
-        cut: 0x01,
-        paste: 0x02
-    };
-    CompositionEvent: {
-        compositionend: 0x00,
-        compositionstart: 0x01,
-        compositionupdate: 0x02
-    };
-    DeviceMotionEvent: {
-        devicemotion: 0x00
-    };
-    DeviceOrientationEvent: {
-        deviceorientation: 0x00
-    };
-    DragEvent: {
-        drag: 0x00,
-        dragend: 0x01,
-        dragenter: 0x02,
-        dragleave: 0x03,
-        dragover: 0x04,
-        dragstart: 0x05,
-        drop: 0x06
-    };
-    Event: {
-        DOMContentLoaded: 0x00,
-        abort: 0x01,
-        afterprint: 0x02,
-        appinstalled: 0x03,
-        audioend: 0x04,
-        audiostart: 0x05,
-        beforeprint: 0x06,
-        blocked: 0x07,
-        canplay: 0x08,
-        canplaythrough: 0x09,
-        change: 0x0A,
-        chargingchange: 0x0B,
-        chargingtimechange: 0x0C,
-        close: 0x0D,
-        complete: 0x0E,
-        devicechange: 0x0F,
-        dischargingtimechange: 0x10,
-        durationchange: 0x11,
-        emptied: 0x12,
-        end: 0x13,
-        ended: 0x14,
-        error: 0x15,
-        fullscreenchange: 0x16,
-        fullscreenerror: 0x17,
-        input: 0x18,
-        invalid: 0x19,
-        languagechange: 0x1A,
-        levelchange: 0x1B,
-        loadeddata: 0x1C,
-        loadedmetadata: 0x1D,
-        offline: 0x1E,
-        online: 0x1F,
-        open: 0x20,
-        orientationchange: 0x21,
-        pause: 0x22,
-        play: 0x23,
-        playing: 0x24,
-        pointerlockchange: 0x25,
-        pointerlockerror: 0x26,
-        ratechange: 0x27,
-        readystatechange: 0x28,
-        reset: 0x29,
-        seeked: 0x2A,
-        seeking: 0x2B,
-        selectionchange: 0x2C,
-        selectstart: 0x2D,
-        slotchange: 0x2E,
-        soundend: 0x2F,
-        soundstart: 0x30,
-        speechend: 0x31,
-        speechstart: 0x32,
-        stalled: 0x33,
-        start: 0x34,
-        submit: 0x35,
-        success: 0x36,
-        suspend: 0x37,
-        timeupdate: 0x38,
-        upgradeneeded: 0x39,
-        versionchange: 0x3A,
-        visibilitychange: 0x3B,
-        voiceschanged: 0x3C,
-        volumechange: 0x3D,
-        waiting: 0x3E
-    };
-    ExtendableMessageEvent: {
-        message: 0x00
-    };
-    FocusEvent: {
-        blur: 0x00,
-        focus: 0x01,
-        focusin: 0x02,
-        focusout: 0x03
-    };
-    GamepadEvent: {
-        gamepadconnected: 0x00,
-        gamepaddisconnected: 0x01
-    };
-    HashChangeEvent: {
-        hashchange: 0x00
-    };
-    KeyboardEvent: {
-        keydown: 0x00,
-        keypress: 0x01,
-        keyup: 0x02,
-        message: 0x03
-    };
-    MessageEvent: {
-        messageerror: 0x00
-    };
-    MouseEvent: {
-        click: 0x00,
-        contextmenu: 0x01,
-        dblclick: 0x02,
-        mousedown: 0x03,
-        mouseenter: 0x04,
-        mouseleave: 0x05,
-        mousemove: 0x06,
-        mouseout: 0x07,
-        mouseover: 0x08,
-        mouseup: 0x09,
-        show: 0x0A
-    };
-    NotificationEvent: {
-        notificationclick: 0x00
-    };
-    OfflineAudioCompletionEvent: {
-        complete: 0x00
-    };
-    PageTransitionEvent: {
-        pagehide: 0x00,
-        pageshow: 0x01
-    };
-    Performance: {
-        resourcetimingbufferfull: 0x00
-    };
-    PointerEvent: {
-        gotpointercapture: 0x00,
-        lostpointercapture: 0x01,
-        pointercancel: 0x02,
-        pointerdown: 0x03,
-        pointerenter: 0x04,
-        pointerleave: 0x05,
-        pointermove: 0x06,
-        pointerout: 0x07,
-        pointerover: 0x08,
-        pointerup: 0x09
-    };
-    PopStateEvent: {
-        popstate: 0x00
-    };
-    ProgressEvent: {
-        abort: 0x00,
-        error: 0x01,
-        load: 0x02,
-        loadend: 0x03,
-        loadstart: 0x04,
-        progress: 0x05,
-        timeout: 0x06
-    };
-    PushEvent: {
-        push: 0x00,
-        pushsubscriptionchange: 0x01
-    };
-    SVGEvent: {
-        SVGAbort: 0x00,
-        SVGError: 0x01,
-        SVGLoad: 0x02,
-        SVGResize: 0x03,
-        SVGScroll: 0x04,
-        SVGUnload: 0x05
-    };
-    SVGZoomEvent: {
-        SVGZoom: 0x00
-    };
-    ServiceWorkerMessageEvent: {
-        message: 0x00
-    };
-    SpeechRecognitionEvent: {
-        nomatch: 0x00,
-        result: 0x01
-    };
-    SpeechSynthesisErrorEvent: {
-        boundary: 0x00,
-        end: 0x01,
-        error: 0x02,
-        mark: 0x03,
-        pause: 0x04,
-        resume: 0x05,
-        start: 0x06
-    };
-    StorageEvent: {
-        storage: 0x00
-    };
-    TimeEvent: {
-        beginEvent: 0x00,
-        endEvent: 0x01,
-        repeatEvent: 0x02
-    };
-    TouchEvent: {
-        touchcancel: 0x00,
-        touchend: 0x01,
-        touchmove: 0x02,
-        touchstart: 0x03
-    };
-    TransitionEvent: {
-        transitionend: 0x00
-    };
-    UIEvent: {
-        abort: 0x00,
-        error: 0x01,
-        load: 0x02,
-        resize: 0x03,
-        scroll: 0x04,
-        select: 0x05,
-        unload: 0x06
-    };
-    UserProximityEvent: {
-        userproximity: 0x00
-    };
-    WheelEvent: {
-        wheel: 0x00
+namespace EventType {
+    export enum AnimationEvent {
+        animationcancel = "animationcancel",
+        animationend = "animationend",
+        animationiteration = "animationiteration",
+        animationstart = "animationstart",
+    }
+    export enum BeforeUnloadEvent {
+        beforeunload = "beforeunload",
+    }
+    export enum ClipboardEvent {
+        copy = "copy",
+        cut = "cut",
+        paste = "paste",
+    }
+    export enum CompositionEvent {
+        compositionend = "compositionend",
+        compositionstart = "compositionstart",
+        compositionupdate = "compositionupdate",
+    }
+    export enum DeviceMotionEvent {
+        devicemotion = "devicemotion",
+    }
+    export enum DeviceOrientationEvent {
+        deviceorientation = "deviceorientation",
+    }
+    export enum DragEvent {
+        drag = "drag",
+        dragend = "dragend",
+        dragenter = "dragenter",
+        dragleave = "dragleave",
+        dragover = "dragover",
+        dragstart = "dragstart",
+        drop = "drop",
+    }
+    export enum Event {
+        abort = "abort",
+        afterprint = "afterprint",
+        appinstalled = "appinstalled",
+        audioend = "audioend",
+        audiostart = "audiostart",
+        beforeprint = "beforeprint",
+        canplay = "canplay",
+        canplaythrough = "canplaythrough",
+        change = "change",
+        chargingchange = "chargingchange",
+        chargingtimechange = "chargingtimechange",
+        close = "close",
+        devicechange = "devicechange",
+        dischargingtimechange = "dischargingtimechange",
+        DOMContentLoaded = "DOMContentLoaded",
+        durationchange = "durationchange",
+        emptied = "emptied",
+        end = "end",
+        ended = "ended",
+        error = "error",
+        fullscreenchange = "fullscreenchange",
+        fullscreenerror = "fullscreenerror",
+        input = "input",
+        invalid = "invalid",
+        languagechange = "languagechange",
+        levelchange = "levelchange",
+        loadeddata = "loadeddata",
+        loadedmetadata = "loadedmetadata",
+        offline = "offline",
+        online = "online",
+        open = "open",
+        orientationchange = "orientationchange",
+        pause = "pause",
+        play = "play",
+        playing = "playing",
+        pointerlockchange = "pointerlockchange",
+        pointerlockerror = "pointerlockerror",
+        ratechange = "ratechange",
+        readystatechange = "readystatechange",
+        reset = "reset",
+        seeked = "seeked",
+        seeking = "seeking",
+        selectionchange = "selectionchange",
+        selectstart = "selectstart",
+        slotchange = "slotchange",
+        soundend = "soundend",
+        soundstart = "soundstart",
+        speechend = "speechend",
+        speechstart = "speechstart",
+        stalled = "stalled",
+        start = "start",
+        submit = "submit",
+        success = "success",
+        suspend = "suspend",
+        timeupdate = "timeupdate",
+        upgradeneeded = "upgradeneeded",
+        visibilitychange = "visibilitychange",
+        voiceschanged = "voiceschanged",
+        volumechange = "volumechange",
+        waiting = "waiting",
+        complete = "complete",
+        versionchange = "versionchange",
+    }
+    export enum ExtendableMessageEvent {
+        message = "message",
+    }
+    export enum FocusEvent {
+        blur = "blur",
+        focus = "focus",
+        focusin = "focusin",
+        focusout = "focusout",
+    }
+    export enum GamepadEvent {
+        gamepadconnected = "gamepadconnected",
+        gamepaddisconnected = "gamepaddisconnected",
+    }
+    export enum HashChangeEvent {
+        hashchange = "hashchange",
+    }
+    export enum IDBVersionChangeEvent {
+        blocked = "blocked",
+    }
+    export enum KeyboardEvent {
+        keydown = "keydown",
+        keypress = "keypress",
+        keyup = "keyup",
+    }
+    export enum MessageEvent {
+        message = "message",
+        messageerror = "messageerror",
+    }
+    export enum MouseEvent {
+        click = "click",
+        contextmenu = "contextmenu",
+        dblclick = "dblclick",
+        mousedown = "mousedown",
+        mouseenter = "mouseenter",
+        mouseleave = "mouseleave",
+        mousemove = "mousemove",
+        mouseout = "mouseout",
+        mouseover = "mouseover",
+        mouseup = "mouseup",
+        show = "show",
+    }
+    export enum NotificationEvent {
+        notificationclick = "notificationclick",
+    }
+    export enum PageTransitionEvent {
+        pagehide = "pagehide",
+        pageshow = "pageshow",
+    }
+    export enum Performance {
+        resourcetimingbufferfull = "resourcetimingbufferfull",
+    }
+    export enum PointerEvent {
+        gotpointercapture = "gotpointercapture",
+        lostpointercapture = "lostpointercapture",
+        pointercancel = "pointercancel",
+        pointerdown = "pointerdown",
+        pointerenter = "pointerenter",
+        pointerleave = "pointerleave",
+        pointermove = "pointermove",
+        pointerout = "pointerout",
+        pointerover = "pointerover",
+        pointerup = "pointerup",
+    }
+    export enum PopStateEvent {
+        popstate = "popstate",
+    }
+    export enum ProgressEvent {
+        abort = "abort",
+        error = "error",
+        load = "load",
+        loadend = "loadend",
+        loadstart = "loadstart",
+        progress = "progress",
+        timeout = "timeout",
+    }
+    export enum PushEvent {
+        push = "push",
+        pushsubscriptionchange = "pushsubscriptionchange",
+    }
+    export enum ServiceWorkerMessageEvent {
+        message = "message",
+    }
+    export enum SpeechRecognitionEvent {
+        nomatch = "nomatch",
+        result = "result",
+    }
+    export enum SpeechSynthesisErrorEvent {
+        error = "error",
+    }
+    export enum SpeechSynthesisEvent {
+        boundary = "boundary",
+        end = "end",
+        mark = "mark",
+        pause = "pause",
+        resume = "resume",
+        start = "start",
+    }
+    export enum StorageEvent {
+        storage = "storage",
+    }
+    export enum SVGEvent {
+        SVGAbort = "SVGAbort",
+        SVGError = "SVGError",
+        SVGLoad = "SVGLoad",
+        SVGResize = "SVGResize",
+        SVGScroll = "SVGScroll",
+        SVGUnload = "SVGUnload",
+    }
+    export enum SVGZoomEvent {
+        SVGZoom = "SVGZoom",
+    }
+    export enum TimeEvent {
+        beginEvent = "beginEvent",
+        endEvent = "endEvent",
+        repeatEvent = "repeatEvent",
+    }
+    export enum TouchEvent {
+        touchcancel = "touchcancel",
+        touchend = "touchend",
+        touchmove = "touchmove",
+        touchstart = "touchstart",
+    }
+    export enum TransitionEvent {
+        transitionend = "transitionend",
+    }
+    export enum UIEvent {
+        abort = "abort",
+        error = "error",
+        load = "load",
+        resize = "resize",
+        scroll = "scroll",
+        select = "select",
+        unload = "unload",
+    }
+    export enum UserProximityEvent {
+        userproximity = "userproximity",
+    }
+    export enum WheelEvent {
+        wheel = "wheel",
     }
 }
-
-enum aUIEvent {
-    abort = 0x00,
-    error = 0x01,
-    load = 0x02,
-    resize = 0x03,
-    scroll = 0x04,
-    select = 0x05,
-    unload = 0x06
-};
-
-aUIEvent;
-
