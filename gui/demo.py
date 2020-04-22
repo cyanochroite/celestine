0a2fd5b318cc40646a52e48527a56309c2760e1b
Mem-Dixy <spammeifyoucan@me.com>
2020-04-22 22:00:50 +0000

    Tutorial
    code from "how to build a gui"
    
    
    Former-commit-id: 42793cbd6728fa572230be5fef69a75652dbc5cb

--- gui/demo.py
+++ gui/demo.py
@@ -1,10 +1,33 @@
-from tkinter import *
-from PIL import ImageTk, Image
+import tkinter
+from PIL import ImageTk
+from PIL import Image
 import os
 
-root = Tk()
+root = tkinter.Tk()
 img = ImageTk.PhotoImage(Image.open(
     "C:\\Users\\mem_d\\Downloads\\external-content.duckduckgo.com.png"))
-panel = Label(root, image=img)
+
+panel = tkinter.Label(root, image=img)
+
 panel.pack(side="bottom", fill="both", expand="yes")
+
+
+label1 = Label(root, text="some text")
+label2 = Label(root, text="syou ugly ")
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=1)
+
+input = Entry(root)
+input.pack()
+
+
+def butt_click():
+    lab = Label(root, text="Moan and groan")
+    lab.pack()
+
+
+butt = Button(root, text="moo", command=butt_click)
+butt.pack()
+
+
 root.mainloop()
