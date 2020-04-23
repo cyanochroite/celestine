7d7171048a88cce13650b0f1c5dbf2bdef1493c6
Mem-Dixy <spammeifyoucan@me.com>
2020-04-23 03:09:34 +0000

    Checkpoint. Stable code.
    Non ideal, but split 'application' out to keep 'window' free of imports.
    
    
    Former-commit-id: b1928f8fda75a1c807fee1aa3f08e2a39f65b9af

---
+++ gui/application.py
@@ -0,0 +1,35 @@
+import tkinter
+import tkinter.ttk
+
+from PIL import ImageTk
+from list import list
+
+
+class application():
+    def __init__(self, image_list):
+        self.tk = tkinter.Tk()
+        self.tk.title("A title")
+        self.tk.iconbitmap("favicon.ico")
+
+        fist = image_list
+        self.image_list = list()
+        self.image_list.add(ImageTk.PhotoImage(fist.get()))
+        fist.next()
+        self.image_list.add(ImageTk.PhotoImage(fist.get()))
+        fist.next()
+        self.image_list.add(ImageTk.PhotoImage(fist.get()))
+        fist.next()
+
+    def _init_button(self, text, command):
+        button = tkinter.Button(self.tk)
+        button["text"] = text
+        button["command"] = command
+        return button
+
+    def _init_label(self, image):
+        label = tkinter.Label(self.tk)
+        label["image"] = image
+        return label
+
+    def mainloop(self):
+        self.tk.mainloop()
