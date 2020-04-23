a789aae4fa41d98bab70dbdfe5cb584c27b97acf
Mem-Dixy <spammeifyoucan@me.com>
2020-04-23 00:20:44 +0000

    seperating functionality
    savepoint for windowing function
    
    
    Former-commit-id: caa9ccc9709121eb01ba51cccff4a65b0cc806f9

--- gui/test.py
+++ gui/test.py
@@ -1,54 +1,27 @@
-from tkinter import *
-from PIL import ImageTk
-from PIL import Image
-
-import functools
-
-root = Tk()
-root.title("A title")
-root.iconbitmap("favicon.ico")
-button_quit = Button(root, text="Exit Program", command=root.quit)
-button_quit.grid(row=0, column=1)
+import tkinter as tk
 
 
-def reset_image():
-    global index
-    global laddel
-    laddel.grid_forget()
-    laddel = Label(image=image_list[index])
-    laddel.grid(row=1, column=0, columnspan=3)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
 
 
-def forward():
-    global index
-    index += 1
-    reset_image()
-    return
-
-
-def back():
-    global index
-    index -= 1
-    reset_image()
-    return
-
-
-icon1 = ImageTk.PhotoImage(Image.open("character.jpg"))
-icon2 = ImageTk.PhotoImage(Image.open("logo.jpg"))
-icon3 = ImageTk.PhotoImage(Image.open("victory.jpg"))
-
-image_list = [icon1, icon2, icon3]
-
-laddel = Label(image=icon1)
-laddel.grid(row=1, column=0, columnspan=3)
-
-back_button = Button(root, text="<<", command=back)
-button_forward = Button(root, text=">>", command=forward)
-
-index = 0
-
-
-back_button.grid(row=2, column=0)
-button_forward.grid(row=2, column=2)
-
-root.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
