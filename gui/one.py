5e90c2efae9a93789e406d7d890bea4347df2560
Mem-Dixy <spammeifyoucan@me.com>
2020-04-24 23:31:32 +0000

    Simplify child constructor.
    Wondering if it even needed.

--- gui/one.py
+++ gui/one.py
@@ -4,14 +4,13 @@ from Frame import Frame
 class one(Frame):
     def __init__(self, master=None, cnf={}, **kw):
         self.data = kw.pop("data", None)
-        self.image_list = self.data.image_list
         super().__init__(master, cnf, **kw)
 
     def _make(self):
         self.button_back = self._init_button("<<", self._button_back)
         self.button_next = self._init_button(">>", self._button_next)
         self.button_quit = self._init_button("Exit Program", self.tk.quit)
-        self.label_screen = self._init_label(self.image_list.get(), 512, 512)
+        self.label_screen = self._init_label(self.data.list_get(), 512, 512)
 
     def _show(self):
         self.button_quit.grid(row=0, column=1)
@@ -20,12 +19,12 @@ class one(Frame):
         self.label_screen.grid(row=1, column=0, columnspan=3)
 
     def reset_image(self):
-        self.label_screen["image"] = self.image_list.get()
+        self.label_screen["image"] = self.data.list_get()
 
     def _button_back(self):
-        self.image_list.back()
+        self.data.list_back()
         self.reset_image()
 
     def _button_next(self):
-        self.image_list.next()
+        self.data.list_next()
         self.reset_image()
--- gui/two.py
+++ gui/two.py
@@ -3,29 +3,19 @@ from Frame import Frame
 
 class two(Frame):
     def __init__(self, master=None, cnf={}, **kw):
-        self.data = kw.pop("data", None)
-        self.image_list = self.data.image_list
         super().__init__(master, cnf, **kw)
+        self.icon = []
 
     def _make(self):
-        self.icon = []
-        for index in range(0, self.image_list._max + 1):
-            get = self.image_list._list[index]
+        for index in range(0, self.data.image_list._max + 1):
+            get = self.data.image_list._list[index]
             image = self._init_label(get, 128, 128)
             image.grid(row=index, column=0)
             self.icon.append(image)
 
     def _show(self):
-        pass
-
-    def reset_image(self):
-        self.label_screen["image"] = self.image_list.get()
-
-    def _button_back(self):
-        self.image_list.back()
-        self.reset_image()
-
-    def _button_next(self):
-        self.image_list.next()
-        self.reset_image()
-
+        for index in range(0, self.data.image_list._max + 1):
+            get = self.data.image_list._list[index]
+            image = self._init_label(get, 128, 128)
+            image.grid(row=index, column=0)
+            self.icon.append(image)
