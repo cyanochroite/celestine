13633d2d757b8f942d5a4e4ef7e6481827c1c54d
Mem-Dixy <spammeifyoucan@me.com>
2020-10-26 04:17:34 +0000

    Star directory conversion working.
    Kind of  a mess but seems to be functional.
    
    
    Former-commit-id: b7da00373435e60f2f4eeabcae9cc912883afe68

--- gui/mem_dixy/star/convert.py
+++ gui/mem_dixy/star/convert.py
@@ -3,47 +3,40 @@ import os
 import hashlib
 
 
-def image_open(path):  # {
+def image_open(path):
     return PIL.Image.open(path)
-# }
 
 
-def image_new(size):  # {
+def image_new(size):
     mode = "RGBA"
 #    size = size;
     color = (0x00, 0x00, 0x00, 0x00)
     return PIL.Image.new(mode, size, color)
-# }
 
 
-def image_save(image, path):  # {
-    if os.path.isfile(path):  # {
+def image_save(image, path):
+    if os.path.isfile(path):
         os.remove(path)
-    # }
-    with open(path, "wb") as file:  # {
+
+    with open(path, "wb") as file:
         image.save(file, "PNG", optimize=True)
-    # }
-# }
 
 
-def file_cypher(path):  # {
+def file_cypher(path):
     cypher = hashlib.sha3_512()
-    with open(path, "rb") as file:  # {
+    with open(path, "rb") as file:
         cypher.update(file.read())
-    # }
+
     return cypher.hexdigest().upper()
-# }
 
 
-def os_rename(open_spot, save_path):  # {
+def os_rename(open_spot, save_path):
     save_spot = save_path + file_cypher(open_spot) + ".png"
-    if not os.path.isfile(save_spot):  # {
+    if not os.path.isfile(save_spot):
         os.rename(open_spot, save_spot)
-    # }
-# }
 
 
-def file_save(load_file):  # {
+def file_save(load_file):
     load_path = ".\\"
 #    load_file = "bleach_yachiru0035-1.jpg";
     save_path = ".\\"
@@ -55,10 +48,10 @@ def file_save(load_file):  # {
     os_rename(save_path + save_file, save_path)
 
 
-# }
 print("scan")
 
 
 file_save("todo.png")
 
 print("done")
+
--- gui/mem_dixy/star/main.py
+++ gui/mem_dixy/star/main.py
@@ -2,36 +2,38 @@ from mem_dixy.package.Pillow.Image import Image
 from mem_dixy.module.hashlib import Hash
 from mem_dixy.module.os import OS
 from mem_dixy.module.os import Path
+from mem_dixy.module.os import File
 
 print("setup")
 
 
-def work(load_this):
-    save_this = Path(file="demo.png").get_file()
+def work(file):
 
+    load_this = OS.join_path("todo", file.full())
+
+    save_this = "demo.png"
     base = Image.open(load_this)
     image = Image.from_input("RGB", base.size, 0)
     image.paste(base)
     image.save(save_this, "PNG")
-
     cypher = Hash.sha3_512(save_this)
 
-    move_this = Path("done", cypher + ".png").get_file()
+    #move_this = Path("done", cypher + ".png").get_file()
+    save = File(cypher + ".png", file.path)
+    print(save.full())
+    move_this = OS.join_path("done", save.full())
 
     OS.rename(save_this, move_this)
 
 
 print("scan")
 
-import os
 
-# current
-dirr = Path()
-dirr.change_directory("todo")
-f = OS.filenames(dirr.get_path())
+(path, file) = OS.chdir("todo", OS.walk_directory)
+OS.chdir("done", OS.makedirs, path)
+for (f) in file:
+    print("convert " + f.name)
+    work(f)
 
-for (path) in f:
-    print("convert " + path)
-    work(path)
 
 print("done")
