9a635f1aea045ce28a76d3ec0ce537fafef12555
mem_dixy <mem_dixy@pm.me>
2022-03-16 02:47:35 +0000

    Reformated pillow.py, moved OS out to module. (It was updated version.)

--- gui/mem_dixy/module/os.py
+++ gui/mem_dixy/module/os.py
@@ -2,38 +2,33 @@ import os
 
 
 class OS:
-    @classmethod
-    def working_directory(cls):
-        # one = Path.Make(os.path.abspath("."))
+    @staticmethod
+    def working_directory():
         return os.getcwd()
 
-    @classmethod
-    def join(cls, path, *paths):
+    @staticmethod
+    def join(path, *paths):
         return os.path.join(path, *paths)
 
-    def remove(cls, path):
+    @staticmethod
+    def remove(path):
         if os.path.isfile(path):
             os.remove(path)
 
-    @classmethod
-    def rename(cls, source, destination):
+    @staticmethod
+    def rename(source, destination):
         if not os.path.isfile(destination):
             os.rename(source, destination)
 
-    @classmethod
-    def filenames(cls, path):
+    @staticmethod
+    def filenames(path):
         file = []
         for (dirpath, dirnames, filenames) in os.walk(path):
             for name in filenames:
                 file.append(os.path.join(dirpath, name))
         return file
 
-    def _image_save(image, path):
-        if os.path.isfile(path):
-            os.remove(path)
-        with open(path, "wb") as file:
-            image.save(file, "PNG", optimize=True)
-
+    @staticmethod
     def walk_directory(top='.'):
         path = []
         file = []
@@ -44,10 +39,12 @@ class OS:
                 file.append((filename, dirpath))
         return (path, file)
 
+    @staticmethod
     def makedirs(paths):
         for path in paths:
             os.mkdir(path)
 
+    @staticmethod
     def chdir(path, call, *args):
         cwd = os.getcwd()
         os.chdir(path)
