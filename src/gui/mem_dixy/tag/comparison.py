c06b2e580229761381083402a99b896b576fc632
mem_dixy <mem_dixy@pm.me>
2022-03-22 06:34:40 +0000

    Swaped some operator order to match what a user what likely type instead of sorting by unicode number.

--- src/gui/mem_dixy/tag/comparison.py
+++ src/gui/mem_dixy/tag/comparison.py
@@ -32,42 +32,41 @@ class comparison(operator):
         return str().join(array)
 
 
-class lt(comparison):
-    primary = comparison.init([LESS_THAN_SIGN])  # _<__
-    secondary = comparison.init([EXCLAMATION_MARK, EQUALS_SIGN, LESS_THAN_SIGN])  # !_=>
-
-
-class le(comparison):
-    primary = comparison.init([LESS_THAN_SIGN, EQUALS_SIGN])  # _<=_
-    secondary = comparison.init([EXCLAMATION_MARK, GREATER_THAN_SIGN])  # !__>
-
-
-class eq(comparison):
+class eq(comparison):  # EQUALITY_OPERATOR
     primary = comparison.init([EQUALS_SIGN])  # __=_
     secondary = comparison.init([EXCLAMATION_MARK, LESS_THAN_SIGN, GREATER_THAN_SIGN])  # !<_>
 
 
-class ne(comparison):
-    primary = comparison.init([EXCLAMATION_MARK, EQUALS_SIGN])  # !_=_
-    secondary = comparison.init([LESS_THAN_SIGN, GREATER_THAN_SIGN])  # _<_>
-
-
-class ge(comparison):
+class ge(comparison):  # GREATER_THAN_OR_EQUAL_OPERATOR
     primary = comparison.init([GREATER_THAN_SIGN, EQUALS_SIGN])  # __=>
     secondary = comparison.init([EXCLAMATION_MARK, GREATER_THAN_SIGN])  # !<__
 
 
-class gt(comparison):
+class gt(comparison):  # GREATER_THAN_OPERATOR
     primary = comparison.init([GREATER_THAN_SIGN])  # ___>
     secondary = comparison.init([EXCLAMATION_MARK, LESS_THAN_SIGN, EQUALS_SIGN])  # !<=_
 
 
-class sa(comparison):
-    primary = comparison.init([])  # ____
-    secondary = comparison.init([LESS_THAN_SIGN, EQUALS_SIGN, GREATER_THAN_SIGN])  # _<=>
+class le(comparison):  # LESS_THAN_OR_EQUAL_OPERATOR
+    primary = comparison.init([LESS_THAN_SIGN, EQUALS_SIGN])  # _<=_
+    secondary = comparison.init([EXCLAMATION_MARK, GREATER_THAN_SIGN])  # !__>
 
 
-class sn(comparison):
+class lt(comparison):  # LESS_THAN_OPERATOR
+    primary = comparison.init([LESS_THAN_SIGN])  # _<__
+    secondary = comparison.init([EXCLAMATION_MARK, EQUALS_SIGN, LESS_THAN_SIGN])  # !_=>
+
+
+class ne(comparison):  # INEQUALITY_OPERATOR
+    primary = comparison.init([EXCLAMATION_MARK, EQUALS_SIGN])  # !_=_
+    secondary = comparison.init([LESS_THAN_SIGN, GREATER_THAN_SIGN])  # _<_>
+
+
+class nn(comparison):  # IS_NOT_NULL_OPERATOR
     primary = comparison.init([EXCLAMATION_MARK])  # !___
     secondary = comparison.init([EXCLAMATION_MARK, LESS_THAN_SIGN, EQUALS_SIGN, GREATER_THAN_SIGN])  # !<=>
 
+
+class nu(comparison):  # IS_NULL_OPERATOR
+    primary = comparison.init([])  # ____
+    secondary = comparison.init([LESS_THAN_SIGN, EQUALS_SIGN, GREATER_THAN_SIGN])  # _<=>
