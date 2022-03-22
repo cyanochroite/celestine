3eef5b7ead0c136ae4a90246cfa3d509b19b5b43
mem_dixy <mem_dixy@pm.me>
2022-03-22 06:34:40 +0000

    Swaped some operator order to match what a user what likely type instead of sorting by unicode number.
    Former-commit-id: c06b2e580229761381083402a99b896b576fc632

--- tests/comparison.py
+++ tests/comparison.py
@@ -8,15 +8,15 @@ from mem_dixy.tag.comparison import *
 
 class check_comparison(unittest.TestCase):
     @classmethod
-    def _add_token(cls, array):
+    def _add_token(cls, array):  # !<>=
         index = 0
         index |= EXCLAMATION_MARK in array
         index <<= 1
         index |= LESS_THAN_SIGN in array
         index <<= 1
-        index |= EQUALS_SIGN in array
-        index <<= 1
         index |= GREATER_THAN_SIGN in array
+        index <<= 1
+        index |= EQUALS_SIGN in array
         return cls.all_encoding.get(index)
 
     def setUp(self):
@@ -25,22 +25,22 @@ class check_comparison(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.all_encoding = {
-            0x0: sa,  # ____
-            0x1: gt,  # ___>
-            0x2: eq,  # __=_
-            0x3: ge,  # __=>
+            0x0: nu,  # ____
+            0x1: eq,  # ___=
+            0x2: gt,  # __>_
+            0x3: ge,  # __>=
             0x4: lt,  # _<__
-            0x5: ne,  # _<_>
-            0x6: le,  # _<=_
-            0x7: sa,  # _<=>
-            0x8: sn,  # !___
-            0x9: le,  # !__>
-            0xA: ne,  # !_=_
-            0xB: lt,  # !_=>
+            0x5: le,  # _<_=
+            0x6: ne,  # _<>_
+            0x7: nu,  # _<>=
+            0x8: nn,  # !___
+            0x9: ne,  # !__=
+            0xA: le,  # !_>_
+            0xB: lt,  # !_>=
             0xC: ge,  # !<__
-            0xD: eq,  # !<_>
-            0xE: gt,  # !<=_
-            0xF: sn,  # !<=>
+            0xD: gt,  # !<_=
+            0xE: eq,  # !<>_
+            0xF: nn   # !<>=
         }
 
     def tearDown(self):
@@ -52,7 +52,7 @@ class check_comparison(unittest.TestCase):
 
     def test__EMPTY(self):
         t = []
-        self.assertIs(self._add_token(t), sa)
+        self.assertIs(self._add_token(t), nu)
 
     def test__EQUALS_SIGN(self):
         t = [EQUALS_SIGN]
@@ -68,7 +68,7 @@ class check_comparison(unittest.TestCase):
 
     def test__EQUALS_SIGN__EXCLAMATION_MARK__GREATER_THAN_SIGN__LESS_THAN_SIGN(self):
         t = [EQUALS_SIGN, EXCLAMATION_MARK, GREATER_THAN_SIGN, LESS_THAN_SIGN]
-        self.assertIs(self._add_token(t), sn)
+        self.assertIs(self._add_token(t), nn)
 
     def test__EQUALS_SIGN__EXCLAMATION_MARK__LESS_THAN_SIGN(self):
         t = [EQUALS_SIGN, EXCLAMATION_MARK, LESS_THAN_SIGN]
@@ -80,7 +80,7 @@ class check_comparison(unittest.TestCase):
 
     def test__EQUALS_SIGN__GREATER_THAN_SIGN__LESS_THAN_SIGN(self):
         t = [EQUALS_SIGN, GREATER_THAN_SIGN, LESS_THAN_SIGN]
-        self.assertIs(self._add_token(t), sa)
+        self.assertIs(self._add_token(t), nu)
 
     def test__EQUALS_SIGN__LESS_THAN_SIGN(self):
         t = [EQUALS_SIGN, LESS_THAN_SIGN]
@@ -88,7 +88,7 @@ class check_comparison(unittest.TestCase):
 
     def test__EXCLAMATION_MARK(self):
         t = [EXCLAMATION_MARK]
-        self.assertIs(self._add_token(t), sn)
+        self.assertIs(self._add_token(t), nn)
 
     def test__EXCLAMATION_MARK__GREATER_THAN_SIGN(self):
         t = [EXCLAMATION_MARK, GREATER_THAN_SIGN]
@@ -114,10 +114,6 @@ class check_comparison(unittest.TestCase):
         t = [LESS_THAN_SIGN]
         self.assertIs(self._add_token(t), lt)
 
-    def test__EQUALS_SIGN__EXCLAMATION_MARK__GREATER_THAN_SIGN__LESS_THAN_SIGN(self):
-        array = [EQUALS_SIGN, EXCLAMATION_MARK, GREATER_THAN_SIGN, LESS_THAN_SIGN]
-        token = self._add_token(array)
-        self.assertIs(token, sn)
 
 
 if __name__ == '__main__':
