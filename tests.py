67c1d3f04e24fa52136aa8a31c9dca80a7f5b4a0
mem_dixy <mem_dixy@pm.me>
2022-04-23 05:36:22 +0000

    Restored main test file.
    Former-commit-id: 6b9b00f41194ebde0fd6385a1ae315c3ab6f3fd2

---
+++ tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from tests.test_parser.test_translator import test_translator
+from tests.test_tag.test_comparison import test_comparison
+from tests.test_tag.test_unary import test_unary
+
+if __name__ == '__main__':
+    unittest.main()
