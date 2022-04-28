92303e2225b9eaa3c86e62adaf9dd3d1d0726151
mem_dixy <mem_dixy@pm.me>
2022-04-28 08:56:21 +0000

    Can now launch two different gui's depending from the command line.
    Former-commit-id: d86967037d23ec9d7763419c6d757be9b4afb3eb

--- src/celestine/main.py
+++ src/celestine/main.py
@@ -16,39 +16,53 @@ TERMINAL = "TERMINAL"
 TKINTER = "TKINTER"
 UNITTEST = "UNITTEST"
 
+
+class package():
+    def __init__(self, name, module, install):
+        self.name = name
+        self.module = module
+        self.install = install
+        self.imported = self._import(module)
+
+    def _import(self, module):
+        try:
+            __import__(module)
+            return True
+        except ModuleNotFoundError:
+            return False
+
+
 PACKAGE = {
-    DEARPYGUI: _import("dearpygui"),
-    PILLOW: _import("PIL"),
-    TKINTER: _import("tkinter"),
-    UNITTEST: _import("unittest")
+    DEARPYGUI: package("DearPyGui", "dearpygui", "pip install dearpygui"),
+    PILLOW: package("Pillow", "PIL", "pip install Pillow"),
+    TKINTER: package("tkinter", "tkinter", "Installing it with python."),
+    UNITTEST: package("unittest", "unittest", "Installing it with python.")
 }
 
 GUI = {
-    TERMINAL: TERMINAL.lower(),
-    DESKTOP: DESKTOP.lower(),
-    TKINTER: TKINTER.lower(),
-    DEARPYGUI: DEARPYGUI.lower()
+    TERMINAL.lower(): "CAT",
+    DESKTOP.lower(): "HAT",
+    TKINTER.lower(): "PIG",
+    DEARPYGUI.lower(): "DOG"
 }
-
-GUI = [
-    TERMINAL.lower(),
-    DESKTOP.lower(),
-    TKINTER.lower(),
-    DEARPYGUI.lower()
-]
-
-
-print(PACKAGE)
 
 
 __version__ = "0.1.2.3"
 
-OK = 0
-ERROR = 1
+
+def check(gui, module):
+    package = PACKAGE[gui]
+    if gui != module:
+        return False
+    if not package.imported:
+        name = package.name
+        install = package.install
+        message = "Package {0} not installed\nTry: {1}".format(name, install)
+        raise SystemExit(message)
+    return True
 
 
-
-def main() -> int:
+def main():
     parser = argparse.ArgumentParser(
         prog="celestine"
     )
@@ -59,6 +73,18 @@ def main() -> int:
         help="Choose a gui to use."
     )
     parse = parser.parse_args()
-    print(parse.gui)
 
-    return OK
+    gui = parse.gui.upper()
+
+    if check(gui, DEARPYGUI):
+        import main_dearpygui
+
+    if check(gui, TKINTER):
+        import main_tkinter
+
+    if check(gui, DESKTOP):
+        pass
+
+    if check(gui, TERMINAL):
+        pass
+
