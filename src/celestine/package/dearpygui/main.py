988665ce2504295913712b2a6845cd9d788b8cb4
mem_dixy <mem_dixy@pm.me>
2022-05-04 01:51:18 +0000

    Merged everything into one file.
    Former-commit-id: 259bc4c8ccafcaeea879ac6350fd7d71ea4bf420

--- src/celestine/package/dearpygui/main.py
+++ src/celestine/package/dearpygui/main.py
@@ -5,6 +5,108 @@ from celestine.package.pillow.ImageFont import ImageFont
 
 #import PIL.Image
 
+
+
+
+
+
+
+
+
+
+############## convert.py
+import PIL.Image;
+import os;
+import hashlib;
+
+def image_open(path):#{
+    return PIL.Image.open(path);
+#}
+
+def image_new(size):#{
+    mode = "RGBA";
+#    size = size;
+    color = (0x00,0x00,0x00,0x00);
+    return PIL.Image.new(mode,size,color);
+#}
+
+def image_save(image, path):#{
+    if os.path.isfile(path):#{
+        os.remove(path);
+    #}
+    with open(path, "wb") as file:#{
+        image.save(file, "PNG", optimize=True);
+    #}
+#}
+        
+def file_cypher(path):#{
+    cypher = hashlib.sha3_512();
+    with open(path, "rb") as file:#{
+        cypher.update(file.read());
+    #}
+    return cypher.hexdigest().upper();
+#}
+    
+def os_rename(open_spot, save_path):#{
+    save_spot = save_path + file_cypher(open_spot) + ".png";
+    if not os.path.isfile(save_spot):#{
+        os.rename(open_spot, save_spot);
+    #}
+#}
+
+def file_save(load_file):#{
+    load_path = "D:/todo/"
+    save_path = "D:/done/";
+    save_file = "demo.png";
+    base = image_open(load_path + load_file);
+    image = image_new(base.size);
+    image.paste(base);
+    image_save(image, save_path + save_file);
+    os_rename(save_path + save_file, save_path);
+#}
+print("scan");
+
+
+#file_save("Control_Screenshot_2.jpg");
+#file_save("Control_Screenshot_3.jpg");
+
+
+print("done");
+
+
+############################ lookup.py
+
+
+import os
+
+path = '.'
+path = os.path.abspath(path)
+
+print(path)
+print(os.path.basename(path))
+print(os.path.dirname(path))
+print(os.path.normpath(path))
+print(os.path.normpath('.'))
+
+import sys
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
+
+mypath = os.path.normpath(path)
+f = []
+for (dirpath, dirnames, filenames) in os.walk(mypath):
+    f.extend(filenames)
+    break
+
+for (path) in f:
+    print("HI " + path)
+
+print(f)
+
+###############################
+
 class Array:
     @staticmethod
     def subdivide(array, subdivision_size):
@@ -288,8 +390,8 @@ def print_path(paths):
         print(item + ",")
 
 #main(paths)
-paths = load_paths()
-print_path(paths)
+#paths = load_paths()
+#print_path(paths)
 
 #dvd(paths)
 
@@ -352,7 +454,93 @@ def callback2(sender, app_data, user_data):
     dpg.configure_item("file_dialog_id", user_data="cat")
     dpg.show_item("file_dialog_id")
 
-    
+
+
+def _create_static_textures():
+
+    # create static textures
+    texture_data1 = []
+    for i in range(100 * 100):
+        texture_data1.append(255 / 255)
+        texture_data1.append(0)
+        texture_data1.append(255 / 255)
+        texture_data1.append(255 / 255)
+
+    texture_data2 = []
+    for i in range(50 * 50):
+        texture_data2.append(255 / 255)
+        texture_data2.append(255 / 255)
+        texture_data2.append(0)
+        texture_data2.append(255 / 255)
+
+    texture_data3 = []
+    for row in range(50):
+        for column in range(50):
+            texture_data3.append(255 / 255)
+            texture_data3.append(0)
+            texture_data3.append(0)
+            texture_data3.append(255 / 255)
+        for column in range(50):
+            texture_data3.append(0)
+            texture_data3.append(255 / 255)
+            texture_data3.append(0)
+            texture_data3.append(255 / 255)
+    for row in range(50):
+        for column in range(50):
+            texture_data3.append(0)
+            texture_data3.append(0)
+            texture_data3.append(255 / 255)
+            texture_data3.append(255 / 255)
+        for column in range(50):
+            texture_data3.append(255 / 255)
+            texture_data3.append(255 / 255)
+            texture_data3.append(0)
+            texture_data3.append(255 / 255)
+
+    dpg.add_static_texture(100, 100, texture_data1, parent="__demo_texture_container", tag="__demo_static_texture_1", label="Static Texture 1")
+    dpg.add_static_texture(50, 50, texture_data2, parent="__demo_texture_container", tag="__demo_static_texture_2", label="Static Texture 2")
+    dpg.add_static_texture(100, 100, texture_data3, parent="__demo_texture_container", tag="__demo_static_texture_3", label="Static Texture 3")
+
+
+def _on_demo_close(sender, app_data, user_data):
+    dpg.delete_item(sender)
+    dpg.delete_item("__demo_texture_container")
+    dpg.delete_item("__demo_colormap_registry")
+    dpg.delete_item("__demo_hyperlinkTheme")
+    dpg.delete_item("__demo_theme_progressbar")
+    dpg.delete_item("stock_theme1")
+    dpg.delete_item("stock_theme2")
+    dpg.delete_item("stock_theme3")
+    dpg.delete_item("stock_theme4")
+    dpg.delete_item("stem_theme1")
+    dpg.delete_item("__demo_keyboard_handler")
+    dpg.delete_item("__demo_mouse_handler")
+    dpg.delete_item("__demo_filedialog")
+    dpg.delete_item("__demo_stage1")
+    dpg.delete_item("__demo_popup1")
+    dpg.delete_item("__demo_popup2")
+    dpg.delete_item("__demo_item_reg3")
+    dpg.delete_item("__demo_item_reg6")
+    dpg.delete_item("__demo_item_reg7")
+    dpg.delete_item("demoitemregistry")
+    for i in range(7):
+        dpg.delete_item("__demo_theme" + str(i))
+        dpg.delete_item("__demo_theme2_" + str(i))
+    for i in range(5):
+        dpg.delete_item("__demo_item_reg1_" + str(i))
+        dpg.delete_item("__demo_item_reg2_" + str(i))
+    for i in range(3):
+        dpg.delete_item("__demo_item_reg4_" + str(i))
+    for i in range(4):
+        dpg.delete_item("__demo_item_reg5_" + str(i))
+
+
+
+def callback2(sender, app_data, user_data):
+    value = dpg.get_value("input")
+    dpg.set_value("print", value)
+
+
 def window():
     dpg.add_file_dialog(directory_selector=True, show=False, callback=callback_file_dialog, tag="file_dialog_id", user_data="cows")
     #
@@ -366,13 +554,145 @@ def window():
         dpg.add_button(label="JPG", callback=callback_test)
         dpg.add_button(label="Star", callback=callback_star)
 
+        dpg.add_texture_registry(label="Demo Texture Container", tag="__demo_texture_container")
+        dpg.add_colormap_registry(label="Demo Colormap Registry", tag="__demo_colormap_registry")
+    
+        with dpg.theme(tag="__demo_hyperlinkTheme"):
+            with dpg.theme_component(dpg.mvButton):
+                dpg.add_theme_color(dpg.mvThemeCol_Button, [0, 0, 0, 0])
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [0, 0, 0, 0])
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [29, 151, 236, 25])
+                dpg.add_theme_color(dpg.mvThemeCol_Text, [29, 151, 236])
+    
+        def _log(sender, app_data, user_data):
+            print(f"sender: {sender}, \t app_data: {app_data}, \t user_data: {user_data}")
+    
+        _create_static_textures()
+    
+        # main stuff
+        dpg.add_input_text(label="moo", tag="input")
+        dpg.add_button(label="Directory Selector", callback=callback2)
+        dpg.add_text(label="Result", tag="print")
+        
+
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                dpg.add_text("Image Button")
+                dpg.add_image_button("__demo_static_texture_1")
+
+            with dpg.group():
+                dpg.add_text("Image")
+                dpg.add_image("__demo_static_texture_2")
+
+            with dpg.group():
+                dpg.add_text("Image (texture size)")
+                dpg.add_image("__demo_static_texture_3")
+
+            with dpg.group():
+                dpg.add_text("Image (2x texture size)")
+                dpg.add_image("__demo_static_texture_3", width=200, height=200)
+
+
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                dpg.add_text("Image Button")
+                dpg.add_image_button("__demo_static_texture_1")
+
+            with dpg.group():
+                dpg.add_text("Image")
+                dpg.add_image("__demo_static_texture_2")
+
+            with dpg.group():
+                dpg.add_text("Image (texture size)")
+                dpg.add_image("__demo_static_texture_3")
+
+            with dpg.group():
+                dpg.add_text("Image (2x texture size)")
+                dpg.add_image("__demo_static_texture_3", width=200, height=200)
+
+
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                dpg.add_text("Image Button")
+                dpg.add_image_button("__demo_static_texture_1")
+
+            with dpg.group():
+                dpg.add_text("Image")
+                dpg.add_image("__demo_static_texture_2")
+
+            with dpg.group():
+                dpg.add_text("Image (texture size)")
+                dpg.add_image("__demo_static_texture_3")
+
+            with dpg.group():
+                dpg.add_text("Image (2x texture size)")
+                dpg.add_image("__demo_static_texture_3", width=200, height=200)
+
+
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                dpg.add_text("Image Button")
+                dpg.add_image_button("__demo_static_texture_1")
+
+            with dpg.group():
+                dpg.add_text("Image")
+                dpg.add_image("__demo_static_texture_2")
+
+            with dpg.group():
+                dpg.add_text("Image (texture size)")
+                dpg.add_image("__demo_static_texture_3")
+
+            with dpg.group():
+                dpg.add_text("Image (2x texture size)")
+                dpg.add_image("__demo_static_texture_3", width=200, height=200)
+
+        with dpg.collapsing_header(label="Node Editor"):
+
+            dpg.add_text("Ctrl+Click to remove a link.", bullet=True)
+
+            with dpg.node_editor(callback=lambda sender, app_data: dpg.add_node_link(app_data[0], app_data[1], parent=sender),
+                                 delink_callback=lambda sender, app_data: dpg.delete_item(app_data)):
+
+                with dpg.node(label="Node 1", pos=[10, 10]):
+
+                    with dpg.node_attribute():
+                        dpg.add_input_float(label="F1", width=150)
+
+                    with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output):
+                        dpg.add_input_float(label="F2", width=150)
+
+                with dpg.node(label="Node 2", pos=[300, 10]):
+
+                    with dpg.node_attribute() as na2:
+                        dpg.add_input_float(label="F3", width=200)
+
+                    with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output):
+                        dpg.add_input_float(label="F4", width=200)
+
+                with dpg.node(label="Node 3", pos=[25, 150]):
+                    with dpg.node_attribute():
+                        dpg.add_input_text(label="T5", width=200)
+                    with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Static):
+                        dpg.add_simple_plot(label="Node Plot", default_value=(0.3, 0.9, 2.5, 8.9), width=200, height=80, histogram=True)
+
 ###########################
 dearpygui.dearpygui.create_context()
-dpg.create_viewport(title='Custom Title', width=1200, height=800)
+dpg.create_viewport(title='Custom Title', width=2600, height=1600)
 dearpygui.dearpygui.setup_dearpygui()
+
+debug=False
+if debug:
+    dearpygui.dearpygui.show_style_editor()
+    dearpygui.dearpygui.show_metrics()
+    dearpygui.dearpygui.show_about()
+    dearpygui.dearpygui.show_debug()
+    dearpygui.dearpygui.show_documentation()
+    dearpygui.dearpygui.show_font_manager()
+    dearpygui.dearpygui.show_item_registry()
 
 window()
 
+dpg.set_primary_window("Main", True)
 
 
 dearpygui.dearpygui.show_viewport(minimized=False, maximized=False)
