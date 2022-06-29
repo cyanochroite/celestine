f02e43dab2c6ad35f11facd77f2174cfbafd7765
mem_dixy <mem_dixy@pm.me>
2022-06-29 08:31:22 +0000

    Remove build files.
    Former-commit-id: deaefe7380bbffcf1845d0eec99a83dcc8c779ea

--- src/pylint.bat
+++
@@ -1,1 +0,0 @@
-python -m pylint --output pylint.txt celestine
--- src/python_3.bat
+++
@@ -1,6 +0,0 @@
-python celestine
-python celestine celestine
-python celestine unittest
-python celestine curses
-python celestine dearpygui
-python celestine tkinter
