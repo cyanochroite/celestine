41120ed098f9d58bbf6bbfa049171edad70cd0b2
mem_dixy <mem_dixy@pm.me>
2022-07-22 18:40:13 +0000

    Integrated external helper applications.
    Former-commit-id: f723f4aa21483d8d919121817928a3138788df33

--- src/celestine/application/configuration.py
+++ src/celestine/application/configuration.py
@@ -1,14 +1,6 @@
 """Generate configuration files for all packages."""
-import os.path
-import sys
-
-directory = os.path.dirname(sys.path[0])
-sys.path.append(directory)
-
 import argparse
-import configparser
 
-from celestine.main.configuration import configuration_load
 from celestine.main.configuration import configuration_save
 from celestine.main.configuration import configuration_celestine
 from celestine.main.configuration import configuration_translator
@@ -16,7 +8,6 @@ from celestine.main.configuration import configuration_translator
 from celestine.keyword.main import CELESTINE
 from celestine.keyword.main import CONFIGURATION_CELESTINE
 from celestine.keyword.main import CONFIGURATION_TRANSLATOR
-
 
 from celestine.keyword.main import LANGUAGE
 from celestine.keyword.main import language
@@ -27,94 +18,112 @@ from celestine.keyword.main import package
 from celestine.keyword.main import PYTHON
 from celestine.keyword.main import python
 
-from celestine.keyword.translator import FILE
+
+class Window():
+    def __init__(self, session):
+        self.session = session
+
+    def file_dialog(self, tag, bind):
+        pass
+
+    def file_dialog_load(self, tag):
+        pass
+
+    def image(self, tag, image):
+        pass
+
+    def image_load(self, file):
+        pass
+
+    def label(self, tag, text):
+        pass
+
+    def run(self, app):
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            "configuration",
+            choices="configuration",
+            help="What is this?",
+        )
+
+        subparser = parser.add_subparsers(
+            title="choice",
+            description="Generate configuration files.",
+            prog="celestine_configuration",
+            dest="choice",
+            required=True,
+            help="Enter the data for the configuration file.",
+        )
+
+        parser_celestine = subparser.add_parser(
+            "celestine",
+            help="Try: english tkinter python_3_10",
+        )
+
+        parser_celestine.add_argument(
+            "language",
+            choices=language,
+            help="The natural language to display the application in.",
+        )
+
+        parser_celestine.add_argument(
+            "package",
+            choices=package,
+            help="Which subpackage to run as the primary application.",
+        )
+
+        parser_celestine.add_argument(
+            "python",
+            choices=python,
+            help="Which version of python to run as.",
+        )
+
+        parser_translator = subparser.add_parser(
+            "translator",
+            help="Try: english tkinter python_3_10",
+        )
+
+        parser_translator.add_argument(
+            "key",
+            help="The natural language to display the application in.",
+        )
+
+        parser_translator.add_argument(
+            "region",
+            help="Which subpackage to run as the primary application.",
+        )
+
+        parser_translator.add_argument(
+            "url",
+            help="Which version of python to run as.",
+        )
+
+        parse = parser.parse_args()
+        configuration = parse.configuration
+        if configuration == "celestine":
+            configuration_save(
+                configuration_celestine(
+                    parse.language,
+                    parse.package,
+                    parse.python,
+                ),
+                directory,
+                CELESTINE,
+                CONFIGURATION_CELESTINE,
+            )
+
+        if configuration == "translator":
+            configuration_save(
+                configuration_translator(
+                    parse.key,
+                    parse.region,
+                    parse.url,
+                ),
+                directory,
+                CELESTINE,
+                CONFIGURATION_TRANSLATOR,
+            )
 
 
-from celestine.keyword.main import language
 
-
-parser = argparse.ArgumentParser(
-    prog="celestine_configuration"
-)
-
-subparser = parser.add_subparsers(
-    title="configuration",
-    description="Generate configuration files.",
-    prog="celestine_configuration",
-    dest="configuration",
-    required=True,
-    help="Enter the data for the configuration file.",
-)
-
-
-parser_celestine = subparser.add_parser(
-    "celestine",
-    help="Try: english tkinter python_3_10",
-)
-
-parser_celestine.add_argument(
-    "language",
-    choices=language,
-    help="The natural language to display the application in.",
-)
-
-parser_celestine.add_argument(
-    "package",
-    choices=package,
-    help="Which subpackage to run as the primary application.",
-)
-
-parser_celestine.add_argument(
-    "python",
-    choices=python,
-    help="Which version of python to run as.",
-)
-
-
-parser_translator = subparser.add_parser(
-    "translator",
-    help="Try: english tkinter python_3_10",
-)
-
-parser_translator.add_argument(
-    "key",
-    help="The natural language to display the application in.",
-)
-
-parser_translator.add_argument(
-    "region",
-    help="Which subpackage to run as the primary application.",
-)
-
-parser_translator.add_argument(
-    "url",
-    help="Which version of python to run as.",
-)
-
-
-parse = parser.parse_args()
-configuration = parse.configuration
-
-if configuration == "celestine":
-    configuration_save(
-        configuration_celestine(
-            parse.language,
-            parse.package,
-            parse.python,
-        ),
-        directory,
-        CELESTINE,
-        CONFIGURATION_CELESTINE,
-    )
-
-if configuration == "translator":
-    configuration_save(
-        configuration_translator(
-            parse.key,
-            parse.region,
-            parse.url,
-        ),
-        directory,
-        CELESTINE,
-        CONFIGURATION_TRANSLATOR,
-    )
--- src/celestine/main/argument.py
+++ src/celestine/main/argument.py
@@ -41,104 +41,16 @@ parser.add_argument(
 )
 
 
-
-subparser = parser.add_subparsers(
-    title="application",
-    description="Generate configuration files.",
-    prog="celestine_configuration",
-    dest=APPLICATION,
-    required=False,
-    help="Enter the data for the configuration file.",
+parser.add_argument(
+    "application",
+    choices=application,
+    default=CELESTINE,
+    help="Tell me which python version you are using.",
 )
 
-
-parser_configuration = subparser.add_parser(
-    "configuration",
-    help="Try: english tkinter python_3_10",
+parser.add_argument(
+    "_",
+    nargs="*",
 )
-
-parser_configuration.add_argument(
-    "language",
-    choices=language,
-    help="The natural language to display the application in.",
-)
-
-parser_configuration.add_argument(
-    "package",
-    choices=package,
-    help="Which subpackage to run as the primary application.",
-)
-
-parser_configuration.add_argument(
-    "python",
-    choices=python,
-    help="Which version of python to run as.",
-)
-
-
-
-
-
-parser_celestine = subparser.add_parser(
-    CELESTINE,
-    help="Help text.",
-)
-
-
-
-parser_curses = subparser.add_parser(
-    CURSES,
-    help="Help text.",
-)
-
-
-
-
-parser_dearpygui = subparser.add_parser(
-    DEARPYGUI,
-    help="Help text.",
-)
-
-
-
-
-parser_tkinter = subparser.add_parser(
-    TKINTER,
-    help="Help text.",
-)
-
-
-
-
-parser_translator = subparser.add_parser(
-    "translator",
-    help="Try: english tkinter python_3_10",
-)
-
-parser_translator.add_argument(
-    "key",
-    help="The natural language to display the application in.",
-)
-
-parser_translator.add_argument(
-    "region",
-    help="Which subpackage to run as the primary application.",
-)
-
-parser_translator.add_argument(
-    "url",
-    help="Which version of python to run as.",
-)
-
-
-
-
-
-parser_unittest = subparser.add_parser(
-    UNITTEST,
-    help="Help text.",
-)
-
-
 
 argument = parser.parse_args()
