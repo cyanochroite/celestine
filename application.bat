d4cd57c1733ed4bea837942cf80cd6cb4550c932
mem_dixy <mem_dixy@pm.me>
2023-04-16 19:13:45 +0000

    Most image loading working.
    Former-commit-id: 8ca5b7a744bfa29111f74a03dbec45280c89bc7b

--- application.bat
+++
@@ -1,4 +0,0 @@
-python ./celestine -l en
-python ./celestine -l en -a demo
-python ./celestine -l en -a viewer
-python ./celestine -l en -a test
--- interface.bat
+++
@@ -1,9 +0,0 @@
-python celestine -a demo -i curses -l en
-python celestine -a demo -i tkinter -l en
-python celestine -a demo -i dearpygui -l en
-python celestine -a demo -i pygame -l en
-
-python celestine -a viewer -i curses -l en
-python celestine -a viewer -i tkinter -l en
-python celestine -a viewer -i dearpygui -l en
-python celestine -a viewer -i pygame -l en
