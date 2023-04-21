3556d44986c64f4114950bb8c5a835fc6964021a
mem_dixy <mem_dixy@pm.me>
2023-04-21 00:10:14 +0000

    Restoring deleted main file.
    Former-commit-id: 17d8bed5f04510916419ff604f2ef02c8fcd2b57

--- celestine/__main__.py
+++ celestine/__main__.py
@@ -1,2 +1,8 @@
 """"""
-from celestine.package.example import *
+import os
+import sys
+
+sys.path[0] = os.path.dirname(sys.path[0])
+
+celestine = __import__("celestine")
+celestine.main(sys.argv[1:], True)
