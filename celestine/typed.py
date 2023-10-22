943ed56226036612b7eec66141a706e978173e14
mem_dixy <mem_dixy@pm.me>
2023-10-22 03:17:58 +0000

    Updating typing file with type.

--- celestine/typed.py
+++ celestine/typed.py
@@ -4,6 +4,7 @@ Define types here.
 Generator[YieldType, SendType, ReturnType]
 """
 
+import typing
 import lzma
 import pathlib
 from collections.abc import Callable as CA
@@ -11,39 +12,60 @@ from collections.abc import Generator as GE
 from collections.abc import Iterable as IT
 from types import ModuleType as MT
 from typing import IO
-from typing import Any as A
-from typing import Dict as D
-from typing import List as L
 from typing import Optional as OPT
 from typing import Self as SELF
 from typing import TextIO
-from typing import Tuple as T
 from typing import Type as TY
 from typing import TypeAlias as TA
-from typing import Union as U
 from typing import override
 
-N: TA = None
-OBJ: TA = object
 
-B: TA = bool
-F: TA = float
-P: TA = pathlib.Path
-S: TA = str
-Z: TA = int
 
-OB: TA = OPT[B]
-OF: TA = OPT[F]
-OP: TA = OPT[P]
-OS: TA = OPT[S]
-OZ: TA = OPT[Z]
 
-LB: TA = L[B]
-LF: TA = L[F]
-LP: TA = L[P]
-LS: TA = L[S]
-LZ: TA = L[Z]
 
+type OBJ = object
+type hd = D[S, A]
+
+
+type A = typing.Any
+type B = bool
+type C = 0
+type D[X] = typing.Dict
+type E = 0
+type F = float
+type G = 0
+type H = 0
+# type I = typing.Any  # Ambiguous variable name.
+type J = 0
+type K = 0
+type L[X] = typing.List
+type M = 0
+type N = None
+# type O = 0  # Ambiguous variable name.
+type P = pathlib.Path
+type Q = 0
+type R = 0
+type S = str
+type T[X] = typing.Tuple
+type U = 0
+type V = 0
+type W = 0
+# type X = 0
+type Y = 0
+type Z = int
+
+
+type OB = OPT[B]
+type OF = OPT[F]
+type OP = OPT[P]
+type OS = OPT[S]
+type OZ = OPT[Z]
+
+type LB = L[B]
+type LF = L[F]
+type LP = L[P]
+type LS = L[S]
+type LZ = L[Z]
 
 class Ignore:
     """"""
@@ -77,6 +99,12 @@ class Ring:
 
 R = Ring  # noqa: F401 pylint: disable=W0611
 
-FILE: TA = IO[A]
-LZMA: TA = U[lzma.LZMAFile, TextIO]
-TEXT: TA = GE[S, N, N]
+type FILE = IO[A]
+type LZMA = lzma.LZMAFile | TextIO
+type TEXT = GE[S, N, N]
+
+
+
+
+
+
