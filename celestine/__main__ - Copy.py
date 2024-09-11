93ac527364adff70130da6bac2f3c88ec243e4c4
mem_dixy <mem_dixy@pm.me>
2024-09-11 03:07:56 +0000

    Simplify number constraints.

---
+++ celestine/__main__ - Copy.py
@@ -0,0 +1,122 @@
+import math
+
+a = 978900
+b = 255310
+c = 221105
+
+a *= math.sqrt(2)
+b *= math.sqrt(2)
+c *= math.sqrt(2)
+
+ba = b / a # 0.2608131576
+ca = c / a # 0.2258708754
+
+testa = 0.0000152
+testb = 0.0000152
+limit = 2500
+
+testa = 0.000044299
+testb = 0.000044299
+limit = 1000
+
+testa = 0.0001
+testb = 0.00001
+limit = 1000
+
+testa = 0.01
+testb = 0.01
+limit = 1000
+
+
+double1 = math.sqrt(2)
+
+shorter1 = 1 / math.sqrt(2)
+oval1 = math.sqrt(2) / math.sqrt(3)
+longer1 = math.pi
+
+
+elipse1 = shorter1 / math.sqrt(3)
+dimond1 = longer1 + double1 + elipse1
+
+lowest =  1.0 / 65536 * 1000
+lowest =  0.1
+
+
+maxlen = (2048 / 2) / math.sqrt(2)
+print(maxlen)
+
+def fix(numer):
+	return abs(round(numer) - numer)
+
+for index in range(1,170):
+	shorter = index * shorter1
+	double = index * double1
+	elipse = index * elipse1
+	oval = index * oval1
+	longer = index * longer1
+	dimond = index * dimond1 + index
+	dimond = 1
+
+	a = fix(index)
+	b = fix(shorter)
+	c = fix(elipse)
+	d = fix(oval)
+	e = fix(longer)
+	f = fix(dimond)
+	g = fix(double)
+
+	a1 = round(index)
+	b1 = round(shorter)
+	c1 = round(elipse)
+	d1 = round(oval)
+	e1 = round(longer)
+	f1 = round(dimond)
+	g1 = round(double)
+
+	g = 0
+	g1 = 0
+	z = a+b+c+d+e+f+g
+	z = a+b+d+g
+
+#	if a < lowest and b<lowest and c<lowest:
+	if z < lowest:
+		print(f"{index}\t{b}\t{b1}\t{d}\t{d1}\t{g}\t{g1}\t{z}")
+		#print(f"{index}\t{a}\t{b}\t{c}\t{d}\t{e}\t{f}\t{g}\t{z}")
+		#print(f"{a1}\t{b1}\t{c1}\t{d1}\t{e1}\t{f1}\t{g1}\t<<")
+
+limit = 0
+tiger = (10,0,0,0)
+for x in range(1,limit):
+	low = int(x*0.2)+1
+	hig = int(x*0.3)+1
+	for y in range(low, hig):
+		if y > x:
+			break
+		for z in range(low, hig):
+			if z > y:
+				break
+
+			x /= math.sqrt(2)
+			y /= math.sqrt(2)
+			z /= math.sqrt(2)
+
+			yx = y/x
+			zx = z/x
+			zy = z/y
+
+			aa = abs(yx - ba)
+			bb = abs(zx - ca)
+
+			all = aa + bb
+			if aa < testa and bb < testb:
+				tiger = (all, aa, bb, x, y, z)
+				print(tiger)
+
+print("done")
+#321 145 556
+# 859 224 194
+#487 127 110
+
+#717 187 162
+#602 157 136
+#487 127 110
--- celestine/__main__.py
+++ celestine/__main__.py
@@ -1,122 +1,59 @@
 import math
 
-a = 978900
-b = 255310
-c = 221105
-
-a *= math.sqrt(2)
-b *= math.sqrt(2)
-c *= math.sqrt(2)
-
-ba = b / a # 0.2608131576
-ca = c / a # 0.2258708754
-
-testa = 0.0000152
-testb = 0.0000152
-limit = 2500
-
-testa = 0.000044299
-testb = 0.000044299
-limit = 1000
-
-testa = 0.0001
-testb = 0.00001
-limit = 1000
-
-testa = 0.01
-testb = 0.01
-limit = 1000
-
-
-double1 = math.sqrt(2)
-
 shorter1 = 1 / math.sqrt(2)
-oval1 = math.sqrt(2) / math.sqrt(3)
-longer1 = math.pi
+double1 = shorter1 * 2.0
+elipse1 = double1 / math.sqrt(3)
+oval1 = 1.0
 
+shorter1 = 1 / math.sqrt(4)
+double1 = 1 / math.sqrt(1)
+elipse1 = 1 / math.sqrt(3)
+oval1 = 1 / math.sqrt(2)
 
-elipse1 = shorter1 / math.sqrt(3)
-dimond1 = longer1 + double1 + elipse1
+longer1 = 1
+dimond1 = 1
 
-lowest =  1.0 / 65536 * 1000
-lowest =  0.1
+mini = 0.251
+lowest = 0.1
 
+mini = 0.5
+lowest = 0.01
 
-maxlen = (2048 / 2) / math.sqrt(2)
-print(maxlen)
-
-def fix(numer):
+def fix(numer: float) -> float:
 	return abs(round(numer) - numer)
 
-for index in range(1,170):
+
+for index1 in range(1,400):
+	index = index1
 	shorter = index * shorter1
 	double = index * double1
 	elipse = index * elipse1
 	oval = index * oval1
 	longer = index * longer1
-	dimond = index * dimond1 + index
-	dimond = 1
+	dimond = index * dimond1
 
 	a = fix(index)
 	b = fix(shorter)
-	c = fix(elipse)
-	d = fix(oval)
-	e = fix(longer)
-	f = fix(dimond)
-	g = fix(double)
+	c = fix(double)
+	d = fix(elipse)
+	e = fix(oval)
+	f = fix(longer)
+	g = fix(dimond)
 
 	a1 = round(index)
 	b1 = round(shorter)
-	c1 = round(elipse)
-	d1 = round(oval)
-	e1 = round(longer)
-	f1 = round(dimond)
-	g1 = round(double)
+	c1 = round(double)
+	d1 = round(elipse)
+	e1 = round(oval)
+	f1 = round(longer)
+	g1 = round(dimond)
 
-	g = 0
-	g1 = 0
 	z = a+b+c+d+e+f+g
-	z = a+b+d+g
 
 #	if a < lowest and b<lowest and c<lowest:
-	if z < lowest:
-		print(f"{index}\t{b}\t{b1}\t{d}\t{d1}\t{g}\t{g1}\t{z}")
-		#print(f"{index}\t{a}\t{b}\t{c}\t{d}\t{e}\t{f}\t{g}\t{z}")
-		#print(f"{a1}\t{b1}\t{c1}\t{d1}\t{e1}\t{f1}\t{g1}\t<<")
-
-limit = 0
-tiger = (10,0,0,0)
-for x in range(1,limit):
-	low = int(x*0.2)+1
-	hig = int(x*0.3)+1
-	for y in range(low, hig):
-		if y > x:
-			break
-		for z in range(low, hig):
-			if z > y:
-				break
-
-			x /= math.sqrt(2)
-			y /= math.sqrt(2)
-			z /= math.sqrt(2)
-
-			yx = y/x
-			zx = z/x
-			zy = z/y
-
-			aa = abs(yx - ba)
-			bb = abs(zx - ca)
-
-			all = aa + bb
-			if aa < testa and bb < testb:
-				tiger = (all, aa, bb, x, y, z)
-				print(tiger)
+	if e < lowest and z < mini:
+		#lowest = z
+		print(f"{index1}\t{a}\t{b}\t{c}\t{d}\t{e}\t{f}\t{g}\t{z}")
+		print(f"{index}\t{a1}\t{b1}\t{c1}\t{d1}\t{e1}\t{f1}\t{g1}\t<<")
 
 print("done")
-#321 145 556
-# 859 224 194
-#487 127 110
-
-#717 187 162
-#602 157 136
-#487 127 110
