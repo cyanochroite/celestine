cdedc60b6d14281f60f50d685df20555d813373e
mem_dixy <mem_dixy@pm.me>
2025-01-24 04:57:55 +0000

    Saving color progress.

--- celestine/__main__.py
+++ celestine/__main__.py
@@ -1,120 +1,12 @@
 """"""
 
-import colorsys
+import importlib
+import os
+import sys
 
-import PIL
-import PIL.Image
+if __name__ == "__main__":
+    path = os.path.dirname(sys.path[0])
+    sys.path.insert(0, path)
 
-
-colours = []
-
-choices = [index * 256 / 7 for index in range(8)]
-crayola = []
-
-
-limit = 8
-
-red = []
-green = []
-blue = []
-cyan = []
-magenta = []
-yellow = []
-grey = []
-
-counter = 0
-for rr in range(limit):
-    for gg in range(limit):
-        for bb in range(limit):
-            counter += 1
-            pixel = (rr, gg, bb)
-            big = max(rr, gg, bb)
-            low = min(rr, gg, bb)
-
-            if rr == gg and gg == bb:
-                grey.append(pixel)
-                continue
-
-            if rr == gg:
-                if bb > rr:
-                    blue.append(pixel)
-                else:
-                    yellow.append(pixel)
-                continue
-
-            if gg == bb:
-                if rr > gg:
-                    red.append(pixel)
-                else:
-                    cyan.append(pixel)
-                continue
-
-            if bb == rr:
-                if gg > bb:
-                    green.append(pixel)
-                else:
-                    magenta.append(pixel)
-                continue
-
-            def findit(aa, oo, cc):
-
-                sorter = [rr, gg, bb]
-                sorter.sort()
-                small = sorter[0]
-                big = sorter[1]
-                isitus = sorter[2]
-                if aa != isitus:
-                    return
-                first = aa - big
-                second = big - small
-                if first > second:
-                    oo.append(pixel)
-                if first < second:
-                    cc.append(pixel)
-                if first == second:
-                    total = rr + gg + bb + 3
-                    compore = limit + limit + limit
-                    if total + total > compore:
-                        cc.append(pixel)
-                    else:
-                        oo.append(pixel)
-
-            findit(rr, red, cyan)
-            findit(gg, green, magenta)
-            findit(bb, blue, yellow)
-
-
-print(counter)
-print(len(red))
-print(len(green))
-print(len(blue))
-print(len(magenta))
-print(len(yellow))
-print(len(cyan))
-print(len(grey))
-
-grouphug = []
-grouphug.extend(red)
-grouphug.extend(green)
-grouphug.extend(blue)
-grouphug.extend(magenta)
-grouphug.extend(yellow)
-grouphug.extend(cyan)
-grouphug.extend(grey)
-
-
-length = len(grouphug) - 1
-image = PIL.Image.new("RGB", (1024, 1024))
-for y in range(1024):
-    for x in range(1024):
-        xx = x // 4
-        yy = min(xx, length)
-        picker = grouphug[yy]
-        colour = (
-            round(choices[picker[0]]),
-            round(choices[picker[1]]),
-            round(choices[picker[2]]),
-        )
-        image.putpixel((x, y), colour)
-
-image.show()
+    celestine = importlib.import_module("celestine")
+    celestine.main(sys.argv[1:], True)
---
+++ celestine/window/cardinal - Copy.py
@@ -0,0 +1,533 @@
+""""""
+
+import itertools
+import math
+import typing
+
+from celestine.literal import (
+    COMMA,
+    FULL_STOP,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS,
+    SPACE,
+    string,
+)
+from celestine.typed import (
+    TS,
+    TS1,
+    TS2,
+    TS3,
+    TS4,
+    VZ,
+    A,
+    B,
+    C,
+    F,
+    K,
+    L,
+    N,
+    S,
+    Struct,
+    T,
+    Z,
+    cast,
+    ignore,
+    override,
+)
+
+type Nomad = typing.Union["Cardinal", F, Z]
+type Unary = C[[Nomad], Nomad]
+type Binary = C[[Nomad, Nomad], Nomad]
+
+
+class Math(Struct):
+    """"""
+
+    @staticmethod
+    def add(one: Nomad, two: Nomad) -> Nomad:
+        """"""
+        result = one + two
+        return result
+
+    @staticmethod
+    def ceil(one: Nomad) -> Nomad:
+        """"""
+        value = math.ceil(one)
+        result = cast(Nomad, value)
+        return result
+
+    @staticmethod
+    def floor(one: Nomad) -> Nomad:
+        """"""
+        value = math.floor(one)
+        result = cast(Nomad, value)
+        return result
+
+    @staticmethod
+    def mul(one: Nomad, two: Nomad) -> Nomad:
+        """"""
+        result = one * two
+        return result
+
+    @staticmethod
+    def neg(one: Nomad) -> Nomad:
+        """"""
+        result = -one
+        return result
+
+    @staticmethod
+    def pos(one: Nomad) -> Nomad:
+        """"""
+        result = +one
+        return result
+
+    @staticmethod
+    def round(one: Nomad, ndigits: VZ = None) -> Nomad:
+        """"""
+        result = round(one, ndigits)
+        return result
+
+    @staticmethod
+    def sub(one: Nomad, two: Nomad) -> Nomad:
+        """"""
+        result = one - two
+        return result
+
+    @staticmethod
+    def truediv(one: Nomad, two: Nomad) -> Nomad:
+        """"""
+        result = one / two
+        return result
+
+    @staticmethod
+    def trunc(one: Nomad) -> Nomad:
+        """"""
+        value = math.trunc(one)
+        result = cast(Nomad, value)
+        return result
+
+
+class Cardinal(Struct):
+    """"""
+
+    __slots__: TS = ("name",)
+
+    name: S
+
+    def binary(self, binary: Binary, other: Nomad) -> L[Nomad]:
+        """Binary arithmetic operations."""
+        data: L[Nomad]
+        if isinstance(other, float | int):
+            data = [other] * len(self.data)
+        else:
+            # TODO: What happens when lists have differnt lengths?
+            # Bigger worry is when SELF is longer and gets cropped.
+            data = getattr(other, "data")
+        result = list(map(binary, self.data, data))
+        return result
+
+    def unary(self, unary: Unary) -> L[Nomad]:
+        """Unary arithmetic operations."""
+        result = list(map(unary, self.data))
+        return result
+
+    def _arithmetic(self, other: Nomad, binary: Binary) -> K:
+        data = self.binary(binary, other)
+        result = self.make(*data)
+        return result
+
+    def _augmented(self, other: Nomad, binary: Binary) -> K:
+        self.data = self.binary(binary, other)
+        return self
+
+    def _inplace(self, unary: Unary) -> K:
+        data = self.unary(unary)
+        result = self.make(*data)
+        return result
+
+    def __add__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.add)
+        return result
+
+    def __ceil__(self) -> K:
+        result = self._inplace(Math.ceil)
+        return result
+
+    def __del__(self) -> N:
+        super().__del__()
+        del self.name
+
+    def __eq__(self, other: object) -> B:
+        if not isinstance(other, Cardinal):
+            return False
+        one = float(self)
+        two = float(other)
+        result = math.isclose(one, two)
+        return result
+
+    def __float__(self) -> F:
+        data = map(float, self.data)
+        result = sum(data)
+        return result
+
+    def __floor__(self) -> K:
+        result = self._inplace(Math.floor)
+        return result
+
+    def __ge__(self, other: Nomad) -> B:
+        one = float(self)
+        two = float(other)
+        result = one >= two
+        return result
+
+    def __gt__(self, other: Nomad) -> B:
+        one = float(self)
+        two = float(other)
+        result = one > two
+        return result
+
+    def __hash__(self) -> Z:
+        number = map(float, self.data)
+        count = sum(number)
+        result = hash(count)
+        return result
+
+    def __iadd__(self, other: Nomad) -> K:
+        result = self._augmented(other, Math.add)
+        return result
+
+    def __imul__(self, other: Nomad) -> K:
+        result = self._augmented(other, Math.mul)
+        return result
+
+    def __isub__(self, other: Nomad) -> K:
+        return self._augmented(other, Math.sub)
+
+    def __itruediv__(self, other: Nomad) -> K:
+        result = self._augmented(other, Math.truediv)
+        return result
+
+    def __le__(self, other: Nomad) -> B:
+        one = float(self)
+        two = float(other)
+        result = one <= two
+        return result
+
+    def __lt__(self, other: Nomad) -> B:
+        one = float(self)
+        two = float(other)
+        result = one < two
+        return result
+
+    def __mul__(self, other: Nomad) -> K:
+        return self._arithmetic(other, Math.mul)
+
+    def __neg__(self) -> K:
+        result = self._inplace(Math.neg)
+        return result
+
+    def __new__(cls, *_: A) -> K:
+        new = super().__new__(cls)
+        name = repr(cls)
+        index = name.rindex(FULL_STOP) + 1
+        new.name = name[index:-2]
+        return new
+
+    def __ne__(self, other: object) -> B:
+        if not isinstance(other, Cardinal):
+            return True
+        one = float(self)
+        two = float(other)
+        result = not math.isclose(one, two)
+        return result
+
+    def __pos__(self) -> K:
+        result = self._inplace(Math.pos)
+        return result
+
+    def __radd__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.add)
+        return result
+
+    def __repr__(self):
+        data = self.__str__()
+        result = string(self.name, data)
+        return result
+
+    def __rmul__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.mul)
+        return result
+
+    def __round__(self, ndigits: VZ = None) -> K:
+        other = itertools.repeat(ndigits)
+        data = list(map(Math.round, self.data, other))
+        result = self.make(*data)
+        return result
+
+    def __rsub__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.sub)
+        return result
+
+    def __rtruediv__(self, other: Nomad) -> K:
+        result = self.__truediv__(other)
+        return result
+
+    def __str__(self):
+        comma = string(COMMA, SPACE)
+        data = comma.join(map(repr, self.data))
+        result = string(LEFT_PARENTHESIS, data, RIGHT_PARENTHESIS)
+        return result
+
+    def __sub__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.sub)
+        return result
+
+    def __truediv__(self, other: Nomad) -> K:
+        result = self._arithmetic(other, Math.truediv)
+        return result
+
+    def __trunc__(self) -> K:
+        result = self._inplace(Math.trunc)
+        return result
+
+
+class Monad[X](Cardinal):
+    """"""
+
+    __slots__: TS1 = ("one",)
+
+    one: X
+
+    @override
+    def _del(self) -> N:
+        del self.one
+
+    @override
+    def _get(self) -> T[X]:
+        return (self.one,)
+
+    @override
+    def _set(self, value: T[X]) -> N:
+        self.one = value[0]
+
+    def __init__(self, one: X) -> N:
+        ignore(self)
+        super().__init__(one)
+
+    def __new__(cls, one: X) -> K:
+        return super().__new__(cls, one)
+
+    data = property(_get, _set, _del)
+
+
+class Dyad[X](Cardinal):
+    """"""
+
+    __slots__: TS2 = ("one", "two")
+
+    one: X
+    two: X
+
+    @override
+    def _del(self) -> N:
+        del self.one
+        del self.two
+
+    @override
+    def _get(self) -> T[X, X]:
+        return (self.one, self.two)
+
+    @override
+    def _set(self, value: T[X, X]) -> N:
+        self.one = value[0]
+        self.two = value[1]
+
+    def __init__(self, one: X, two: X) -> N:
+        ignore(self)
+        super().__init__(one, two)
+
+    def __new__(cls, one: X, two: X) -> K:
+        return super().__new__(cls, one, two)
+
+    data = property(_get, _set, _del)
+
+
+class Triad[X](Cardinal):
+    """"""
+
+    __slots__: TS3 = ("one", "two", "tri")
+
+    one: X
+    two: X
+    tri: X
+
+    @override
+    def _del(self) -> N:
+        del self.one
+        del self.two
+        del self.tri
+
+    @override
+    def _get(self) -> T[X, X, X]:
+        return (self.one, self.two, self.tri)
+
+    @override
+    def _set(self, value: T[X, X, X]) -> N:
+        self.one = value[0]
+        self.two = value[1]
+        self.tri = value[2]
+
+    def __init__(self, one: X, two: X, tri: X) -> N:
+        ignore(self)
+        super().__init__(one, two, tri)
+
+    def __new__(cls, one: X, two: X, tri: X) -> K:
+        return super().__new__(cls, one, two, tri)
+
+    data = property(_get, _set, _del)
+
+
+class Tetrad[X](Cardinal):
+    """"""
+
+    __slots__: TS4 = ("one", "two", "tri", "tet")
+
+    one: X
+    two: X
+    tri: X
+    tet: X
+
+    @override
+    def _del(self) -> N:
+        del self.one
+        del self.two
+        del self.tri
+        del self.tet
+
+    @override
+    def _get(self) -> T[X, X, X, X]:
+        return (self.one, self.two, self.tri, self.tet)
+
+    @override
+    def _set(self, value: T[X, X, X, X]) -> N:
+        self.one = value[0]
+        self.two = value[1]
+        self.tri = value[2]
+        self.tet = value[3]
+
+    def __init__(self, one: X, two: X, tri: X, tet: X) -> N:
+        ignore(self)
+        super().__init__(one, two, tri, tet)
+
+    def __new__(cls, one: X, two: X, tri: X, tet: X) -> K:
+        return super().__new__(cls, one, two, tri, tet)
+
+    data = property(_get, _set, _del)
+
+
+#######
+
+from celestine.typed import Z, TF3
+import colorsys
+import math
+import PIL
+import PIL.Image
+
+type Z = int
+
+colours = []
+
+choices = [index * 256 / 6 for index in range(7)]
+crayola = []
+
+type PIXELS = L[TF3]
+
+
+def makeit(limit: Z) -> PIXELS:
+    """"""
+    result: PIXELS = []
+    step = limit / 15
+    for index in range(16):
+        scale = index * step
+        number = round(scale)
+        pixel = (number, number, number)
+        result.append(pixel)
+    return result
+
+
+candy1 = makeit(255)
+candy2 = makeit(1000)
+
+limit = 7
+
+
+def add_greys(greys: Z) -> PIXELS:
+    """"""
+    result: PIXELS = []
+    for grey in range(greys):
+        pixel = Triad(grey, grey, grey)
+        result.append(pixel)
+
+    return result
+
+
+def add_colours(colours: Z) -> PIXELS:
+    """"""
+    cutoff = math.floor(colours / 2 - 1)
+    result: PIXELS = []
+    for red in range(colours):
+        for green in range(colours):
+            for blue in range(colours):
+                pixel = Triad(red, green, blue)
+                maximum = max(pixel)
+                minimum = min(pixel)
+                chroma = maximum - minimum
+                if chroma > cutoff:
+                    result.append(pixel)
+
+    return result
+
+
+def add_pixels(function, limit: Z) -> PIXELS:
+    """"""
+
+    def scale(pixel: Triad) -> TF3:
+        result = pixel / (limit - 1)
+        return result
+
+    pixels = function(limit)
+    scale = map(scale, pixels)
+    result = list(scale)
+    return result
+
+
+def add_all(function, limit: Z) -> PIXELS:
+    """"""
+    result: PIXELS = []
+    greys = add_pixels(add_greys, 16)
+    colours = add_pixels(add_colours, 7)
+    result.extend(greys)
+    result.extend(colours)
+    return result
+
+
+doggy = add_all(add_greys, 16)
+print(doggy)
+
+crayola = list(set(crayola))
+
+length = len(crayola) - 1
+image = PIL.Image.new("RGB", (1024, 1024))
+for y in range(1024):
+    for x in range(1024):
+        xx = x // 4
+        yy = min(xx, length)
+        picker = crayola[yy]
+        colour = (
+            round(choices[picker[0]]),
+            round(choices[picker[1]]),
+            round(choices[picker[2]]),
+        )
+        image.putpixel((x, y), colour)
+
+image.show()
